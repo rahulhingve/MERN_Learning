062a3a18ba944ecc9bd1b8ab48136cf4ee1c764f
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-01 22:43:41 +0530

    Uploading local class codes to git

---
+++ class-1/main.js
@@ -0,0 +1,23 @@
+// let firstName = "rahul";
+// let age = 24;
+// let married = false;
+
+
+// console.log("name is "+ firstName +" and age is "+age);
+
+// two sum here 
+
+// function sum (a,b){
+//     const sumValue=(a+b);
+//     return sumValue;
+// }
+// const value  = sum (1,2);
+// console.log(value);
+
+let sum =0 ; 
+
+ for ( let i= 0 ; i<1000000000000; i++){
+    sum = sum+i; 
+    console.log(sum);
+ }
+  
