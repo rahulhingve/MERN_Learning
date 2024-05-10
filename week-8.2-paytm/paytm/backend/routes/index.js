3dede47141b8ff265ad027de021e67c9ce4d9f5b
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-10 16:24:06 +0530

    added signin point

--- week-8.2-paytm/paytm/backend/routes/index.js
+++ week-8.2-paytm/paytm/backend/routes/index.js
@@ -2,18 +2,9 @@ const express = require("express");
 const zod = require("zod");
 const userRoute = require("./user")
 const router = express.Router();
-app.use(express.json());
-app.use("api/v1/user",userRoute)
-
-
-app.post("/signup",function (req , res){
- const username = zod.string(req.body.username).email();
- const firstName =zod.string(req.body.username);
- const lastName = zod.string(req.body.username);
- const password = zod.string(req.body.username).min(8);
+app.use("api/v1/user", userRoute)
 
 
 
-})
 
-module.exports=router;
+module.exports = router;
--- week-8.2-paytm/paytm/backend/routes/user.js
+++ week-8.2-paytm/paytm/backend/routes/user.js
@@ -1,5 +1,67 @@
 const express = require("express");
+const zod = require("zod");
+const {User} = require("../db")
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+app.use(express.json());
 
-const route = express.Router();
 
-module.exports=route;
+const signUpScheme = zod.object({
+    username: zod.string().email(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password: zod.string().min(8)
+
+})
+
+router.post("/signup", async function (req, res) {
+
+    const body = req.body;
+
+
+    // this is very high lever hard to grasp
+
+    // const {success} = signUpScheme.safeParse(req.body);
+
+    const parsedResult = signUpScheme.safeParse();
+    const success = parsedResult.success();
+    if (!success) {
+        return res.status(411).json({
+
+            message: "Incorrect inputs"
+        })
+    }
+
+    const exUser = await User.findOne({
+        username : body.username
+    })
+
+    if(exUser){
+        return res.status(411).json({
+
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+
+    const userId = user._id;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET)
+
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
+
+})
+
+module.exports=router;
