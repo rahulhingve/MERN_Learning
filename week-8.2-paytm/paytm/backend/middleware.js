6f2fb70c5d72f71bcdbf4412e0ecc654dc4eaf1a
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-10 17:16:25 +0530

    middleware

---
+++ week-8.2-paytm/paytm/backend/middleware.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./config");
+
+app.use(express.json());
+
+
+
+
+
+
+
+const authMiddleware = (req, res, next) => {
+
+    const auth = req.headers.authorization;
+
+    if (!auth || auth.startsWith('Bearer ')) {
+        return res.status(403).json({});
+
+    }
+
+    const token = auth.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        if (decoded.userId) {
+            req.userId = decoded.userId;
+            next();
+
+        }
+    } catch (e) {
+        res.status(403).json({})
+    }
+
+
+}
+module.exports = {
+    authMiddleware
+}
