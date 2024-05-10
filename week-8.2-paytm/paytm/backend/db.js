9175f72588d8f760e6d9e0931225aa2646be71c9
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-10 21:54:32 +0530

    Account Schema Added in DataBase

--- week-8.2-paytm/paytm/backend/db.js
+++ week-8.2-paytm/paytm/backend/db.js
@@ -11,12 +11,26 @@ const userSchema =  new mongoose.Schema({
 
 const User = new mongoose.model("User", userSchema);
 
+
+const AccountSchema = new mongoose.Schema({
+    userId :{
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    },
+})
+const Account = mongoose.model("Account",AccountSchema)
+
 //  i can either use this 
-
-
-module.exports= User;
+// module.exports= {User};
 
 // but its used for only one module to export so below there is better way to export it 
-module.export = {
-    User
+module.exports = {
+    User,
+    Account
+    
 }
