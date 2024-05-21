60cd696455b2828068c58538b8f5f28d756a760b
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-21 13:12:41 +0530

    more code

--- week-10.2-prisma-orm/prisma/src/index.ts
+++ week-10.2-prisma-orm/prisma/src/index.ts
@@ -2,17 +2,30 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function insertUser(username:string,password:string,firstName:string,lastName:string){
+// async function insertUser(username:string,password:string,firstName:string,lastName:string){
 
-const res = await  prisma.user.create({
-    data: {
-        email: username,
-        password,
-        firstName,
-        lastName
+// const res = await  prisma.user.create({
+//     data: {
+//         email: username,
+//         password,
+//         firstName,
+//         lastName
+//     }
+// })
+// console.log(res)
+
+// }
+// insertUser("rahul1@gmail.com","password", "rahul","pawar")
+
+async function findUser(username:string){
+
+const res = await prisma.user.findFirst({
+    where:{
+        firstName : username
     }
+    
 })
 console.log(res)
-
 }
-insertUser("rahul1@gmail.com","password", "rahul","pawar")
+
+findUser("rahul")
