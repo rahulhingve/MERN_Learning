7817c178452736ac7d1f8a09130eed0094e5d2cf
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-29 10:37:28 +0530

    added with user schema

---
+++ week-12.5/Todo-Assignment-Prisma/prisma/migrations/20240529050531_initalize_user/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
