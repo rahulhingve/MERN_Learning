3ac0199f3cdbd4aecca97b7207d333a3e6c919c2
rahulhingve <rahulpawar2001.rp@gmail.com>
2024-05-31 17:02:14 +0530

    Added Middleware And route Handler

--- week-13/medium-blog/backend/src/index.ts
+++ week-13/medium-blog/backend/src/index.ts
@@ -1,138 +1,22 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode, sign, verify } from 'hono/jwt'
 
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string
     SECRET_KEY: string
+
+  },
+  Variables: {
+    userId: string
   }
+
 }>()
 
-app.use('/api/v1/blog/*', async(c,next) => {
 
-
-try{
-  const header =  c.req.header("authorization");
-  if(!header){
-    return c.json({
-      error:"Plz Sign IN Again"
-    })
-  }
-  
-  const response = await verify(header,c.env.SECRET_KEY)
-  
-  if(response.id){
-    next();
-  }else{
-    c.status(403)
-    return c.json({
-      error :"Unauthorized"
-    })
-  }
-}catch(e){
-  c.status(403)
-  return c.json({
-    error:"Wrong Authorization Sign In Again"
-  })
-}
-
-
-
-
-   
-})
-
-app.post('/api/v1/user/signup', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-
-  const body = await c.req.json()
-
-  try {
-    const user = await prisma.user.create({
-      data: {
-        name: body.name,
-        email: body.email,
-        password: body.password
-
-
-      }
-    });
-
-    const jwt = await sign({ id: user.id }, c.env.SECRET_KEY);
-    return c.json({ jwt })
-  } catch (e) {
-    c.status(403);
-    return c.json({
-      error: "error while sign up"
-    });
-  }
-
-
-
-
-})
-
-
-app.post('/api/v1/user/signin', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-
-  const body = await c.req.json();
-try{
-  const user = await prisma.user.findUnique({
-    where:{email:body.email,password:body.password}
-   })
-   if(!user){
-    return c.json({
-      error:"wrong email and password"
-    })
-   }
-
-   const token  = await sign({id:user.id},c.env.SECRET_KEY)
-   return c.json({
-    token
-   })
-}catch(e){
-  return c.json({
-    error:"something went wrong"
-  })
-}
-
-
-
-})
-
-
-
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('post blog Hono!')
-})
-
-
-
-app.put('/api/v1/blog', (c) => {
-  return c.text('put  blog!')
-})
-
-
-app.get('/api/v1/blog/bulk', (c) => {
-  return c.text('bulk')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-  return c.text('idddddd!')
-})
-
-
-
+app.route('/api/v1/user',userRouter);
+app.route('/api/v1/blog',blogRouter);
 
 export default app
--- week-13/medium-blog/backend/src/routes/blog.ts
+++ week-13/medium-blog/backend/src/routes/blog.ts
@@ -1,3 +1,151 @@
 import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
-export const blogRouter = new Hono();
+export const blogRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        SECRET_KEY: string;
+
+    },
+    Variables: {
+        userId: string;
+    }
+}>();
+
+
+// midddleware ///////////////////
+
+blogRouter.use('/*', async (c, next) => {
+
+    try {
+        const header = c.req.header("authorization");
+        if (!header) {
+            return c.json({
+                error: "Plz Sign IN Again"
+            })
+        }
+
+        const response = await verify(header, c.env.SECRET_KEY)
+
+        if (response.id) {
+            c.set("userId", response.id as string)
+            await next();
+        } else {
+            c.status(403)
+            return c.json({
+                error: "Unauthorized"
+            })
+        }
+    } catch (e) {
+        c.status(403)
+        return c.json({
+            error: "Wrong Authorization Sign In Again"
+        })
+    }
+
+})
+
+
+blogRouter.post('/', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const userId = c.get("userId")
+    try {
+        const post = await prisma.post.create({
+            data: {
+                title: body.title,
+                content: body.content,
+                published: false,
+                authorId: userId
+            }
+        })
+        return c.json({
+            id: post.id
+        })
+    } catch (e) {
+        return c.json({
+            error: "I think UserId nahi mili from Post.blog Route"
+        })
+    }
+
+
+})
+
+
+
+blogRouter.put('/', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+
+    try {
+        const updateBlogPost = await prisma.post.update({
+            where: { id: body.id },
+            data: {
+                title: body.title,
+                content: body.content
+            }
+        })
+        return c.json({
+            id: updateBlogPost.id
+        })
+    } catch (e) {
+        return c.json({
+            error: "Pata nahi kya hua Sahi input do .this is put route of blog "
+        })
+    }
+
+})
+blogRouter.get('/bulk', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+
+
+    try {
+        const blogTitle = await prisma.post.findMany();
+        return c.json({
+            blogTitle
+        })
+
+    } catch (e) {
+        return c.json({
+            error: "Bhai isme kyu error aagaya teko get router from blog/bulk"
+        })
+    }
+})
+
+blogRouter.get('/:id', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const id = await c.req.param("id");
+
+    try {
+        const blog = await prisma.post.findFirst({
+            where: { id: id }
+
+        })
+        return c.json({
+            blog
+        })
+    } catch (e) {
+        return c.json({
+            error: "ig post id nahi mili this is get router from /blog"
+        })
+    }
+})
+
+
+
+
