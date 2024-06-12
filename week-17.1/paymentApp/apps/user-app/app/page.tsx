ba0cf7d269f18eebc36c67cdc3e6e1e5b3a653b4
unknown <rahulpawar2001.rp@gmail.com>
2024-06-12 19:27:14 +0530

    Setting up New Projects

---
+++ week-17.1/paymentApp/apps/user-app/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-xl font-bold">
+      hello rahul
+    </div>
+  );
+}
