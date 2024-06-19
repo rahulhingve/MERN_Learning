7f0dcdf2f064e68a52339d9ae9df3b5ad3c4d8ac
unknown <rahulpawar2001.rp@gmail.com>
2024-06-19 16:05:12 +0530

    redis worker

---
+++ week-19.4/worker/src/index.ts
@@ -0,0 +1,15 @@
+import { createClient } from "redis"
+
+const client = createClient();
+
+async function main() {
+    await client.connect();
+    while (1) {
+        const response = await client.brPop("submissions", 0)
+        console.log(response)
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+        console.log("process users subbmissoions")
+    }
+}
+
+main()
